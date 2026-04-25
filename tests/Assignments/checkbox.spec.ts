3bc3677de7abfc75f31fb48dc63d9234b54785f1
aishwarya207 <84499461+aishwarya207@users.noreply.github.com>
2026-04-25 10:51:41 +0000

    finalize project setup

---
+++ tests/Assignments/checkbox.spec.ts
@@ -0,0 +1,18 @@
+import{test, expect} from '@playwright/test';
+
+test('checkboxes state', async({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/checkboxes');
+
+    const checkbox1=page.locator('input[type="checkbox"]').nth(0);
+    const checkbox2=page.locator('input[type="checkbox"]').nth(1);
+
+    await checkbox1.check();
+    await checkbox2.check();
+
+    await expect(checkbox1).toBeChecked();
+    await expect(checkbox2).toBeChecked();
+
+    await checkbox1.uncheck();
+
+    await expect(checkbox1).not.toBeChecked();
+});
