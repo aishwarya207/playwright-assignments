4c07b6f0ae8369b994795a3c71cc91092b702b54
aishwarya207 <84499461+aishwarya207@users.noreply.github.com>
2026-04-26 12:06:53 +0000

    finalize project

--- tests/Assignments/hover.spec.ts
+++ tests/Assignments/hover.spec.ts
@@ -1,5 +1,22 @@
 import { test, expect} from "@playwright/test";
 test('hover effects', async({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/hovers');
+    const users=page.locator('.figure');
+    for(let i=0;i<3;i++) {
+        const user=users.nth(i);
+        await user.hover();
+        const caption=user.locator('.figcaption h5');
+        await expect(caption).toBeVisible();
+        await expect(caption).toContainText('user${i+1}');
+        const profileLink=user.locator('text=View Profile');
+        await profileLink.click();
+        await expect(page).toHaveURL('https://the-internet.herokuapp.com.users/${i+1}');
+        await expect(page.locator('h1')).toHaveText('Not Found');
+        await page.goBack();
+        
+
+        
+    }
     
 
 });
